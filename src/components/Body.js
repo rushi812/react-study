d3caf30fa90c0acbc9249bb28d66138b9f3b7a30
rushi812 <rushibrahmbhatt33@gmail.com>
2024-05-18 15:45:18 +0530

    Integrated restaurants api and UI changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,43 @@
 import RestaurantCard from "./RestaurantCard";
-import RES_LIST from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [resList, setResList] = useState(RES_LIST);
+  const [resList, setResList] = useState([]);
 
   const handleFilterRatings = () => {
     const filteredList = resList.filter((res) => res.info.avgRating > 4);
     setResList(filteredList);
   };
 
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.022505&lng=72.5713621&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const res = await data.json();
+    setResList(
+      res?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="body">
       {/* <div className="search">Search...</div> */}
       <div className="filter-container">
         <button className="filter-btn" onClick={handleFilterRatings}>
-          Ratings 4+
+          ⭐️ Ratings 4+
         </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
-          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
-        ))}
+        {!resList.length
+          ? Array(10).fill(<Shimmer />)
+          : resList.map((restaurant) => (
+              <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+            ))}
       </div>
     </div>
   );
---
+++ src/components/Shimmer.js
@@ -0,0 +1,15 @@
+const Shimmer = () => {
+  return (
+    <div className="shimmer-card">
+      <div className="shimmer-image" />
+      <div className="shimmer-info">
+        <div className="info" />
+        <div className="info" />
+        <div className="info" />
+        <div className="info" />
+      </div>
+    </div>
+  );
+};
+
+export default Shimmer;
